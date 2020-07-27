44f9f74cf11adbe946ee43ecff796ffaa6f52d26
Damien Laffond <damien.laffond@nventive.com>
2020-07-27 10:20:36 -0400

    change title

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Link to="/">Home chocopepito </Link>
+          <Link to="/">Home FINAL KUB </Link>
           <Link to="/otherpage">otherpage</Link>
         </header>
          <div>
